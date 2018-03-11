5128076c88a1af670737602ddd8de0a4916729e2
lenastar <lena.starkova.1998@mail.ru>
2018-03-11 14:19:13 +0300

    Update homeworks

--- hw.c
+++ hw.c
@@ -1,9 +1,21 @@
 #include <stdio.h>
 
 int main(void){
-	float a,b,c;
-	scanf("%f %f %f",&a,&b,&c);
-	if ((a * a + b * b == c * c) || (a * a + c * c == b * b) || (c * c + b * b == a * a)){
+	float a;
+	float b;
+	float c;
+	scanf("%f %f %f", &a, &b, &c);
+	if (a + b < c || a + c < b || b + c < a){
+		puts("Uncorrect");
+		return 1;
+	}
+	else if (a * a + b * b == c * c){
+		puts("True");
+	}
+	else if (a * a + c * c == b * b){
+		puts("True");
+	}
+	else if (c * c + b * b == a * a){
 		puts("True");
 	}
 	else{
--- hw2.c
+++ hw2.c
@@ -1,19 +1,19 @@
 #include <stdio.h>
 
 int main(void){
-	int count_digital=0;
-	int count_character=0;
+	int count_digital = 0;
+	int count_character = 0;
 	char c;
 	
 	while ((c = getc(stdin)) != EOF){
-		if (c >= 'a' && c <= 'z' || c >= 'A' && c <= 'Z'){
-			count_character+=1;
+		if ((c >= 'a' && c <= 'z') || (c >= 'A' && c <= 'Z')){
+			count_character += 1;
 		}
 		if (c>='0' && c<='9'){
-			count_digital+=1;
+			count_digital += 1;
 		}
 	}
-	printf("Count of digital:%d\n",count_digital);
-	printf("Count of character:%d",count_character);
+	printf("Count of digital:%d\n", count_digital);
+	printf("Count of character:%d", count_character);
 	
 }
